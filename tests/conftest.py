163e0c1aee421fbeb2881abfe7b631c88667275a
ericsalesdeandrade <sdaeric19@gmail.com>
2024-05-01 08:54:25 +0100

    docs

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,16 +6,19 @@ import pytest
 
 @pytest.fixture
 def test_client():
+    """Return a FastAPI test client instance."""
     yield TestClient(app)
 
 
 @pytest.fixture
 def user_id(scope="session"):
+    """Generate a random user id."""
     return str(uuid.uuid4())
 
 
 @pytest.fixture
 def user_payload(user_id):
+    """Generate a user payload."""
     return {
         "id": user_id,
         "first_name": "PLACEHOLDER",
